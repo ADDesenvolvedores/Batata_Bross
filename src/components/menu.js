731b32e5129f8a3a8375fb578d3bd4cba41a52dd
AlexandreHiroyuki <alexandrehiroyuki@hotmail.com>
2018-10-27 23:37:22 -0300

    Update Layout

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,25 @@
 var gameConfig = new Vue({
   el: "#game-config",
   data: {
+    show: false,
     width: 800,
     height: 600
+  },
+  methods: {
+    toggle: function() {
+      this.show = !this.show;
+    }
+  }
+});
+
+var titleScreen = new Vue({
+  el: "#title-screen",
+  data: {
+    show: true
+  },
+  methods: {
+    toggle: function() {
+      this.show = !this.show;
+    }
   }
 });
